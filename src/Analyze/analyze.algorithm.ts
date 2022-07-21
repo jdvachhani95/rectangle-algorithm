c99d8e30328176f11437e88a46921570d4aed29b
jdvachhani95 <jaydeepvachhani22@gmail.com>
2022-07-20 20:58:16 -0400

    updated form of function variable

--- src/Analyze/analyze.algorithm.ts
+++ src/Analyze/analyze.algorithm.ts
@@ -10,13 +10,21 @@ export class AnalyzeAlgorithm {
   // (E,F)=> Lowest x & y point of Rectangle2
   // (G,H)=> Highest x & y point of Rectangle2
 
-  analyzeAlignment(rectanglesCoordinates: RectanglesInput): string {
+  analyzeAlignment(
+    A: number,
+    B: number,
+    C: number,
+    D: number,
+    E: number,
+    F: number,
+    G: number,
+    H: number
+  ): string {
     const possibilities = [
       "Adjacent (Proper)",
       "Adjacent (Sub-line)",
       "Adjacent (Partial)",
     ];
-    const { A, B, C, D, E, F, G, H } = rectanglesCoordinates;
     switch (true) {
       case E === C || G === A:
         switch (true) {
@@ -41,16 +49,23 @@ export class AnalyzeAlgorithm {
     }
   }
 
-  getAnalyzedResult(rectanglesCoordinates: RectanglesInput): string {
-    const { A, B, C, D, E, F, G, H } = rectanglesCoordinates;
-    console.log(rectanglesCoordinates);
+  getAnalyzedResult(
+    A: number,
+    B: number,
+    C: number,
+    D: number,
+    E: number,
+    F: number,
+    G: number,
+    H: number
+  ): string {
     switch (true) {
       case E > C || F > D || A > G || B > H:
         return "No Intersection";
       case A <= E && G <= C && B <= F && H <= D:
         return "Containment";
       case E === C || F === D || A === G || B === H:
-        return this.analyzeAlignment(rectanglesCoordinates);
+        return this.analyzeAlignment(A, B, C, D, E, F, G, H);
       case E < C || F < D || A < G || B < H:
         return "Intersection";
     }
