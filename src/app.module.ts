f93ffc511a2767428b61a31d269299c133045b98
jdvachhani95 <jaydeepvachhani22@gmail.com>
2022-07-20 21:36:11 -0400

    updated core module to include area GET endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { AnalyzeModule } from "./analyze/analyze.module";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { AreaModule } from "./area/area.module";
 
 @Module({
-  imports: [AnalyzeModule],
+  imports: [AnalyzeModule, AreaModule],
   controllers: [AppController],
   providers: [AppService],
 })
